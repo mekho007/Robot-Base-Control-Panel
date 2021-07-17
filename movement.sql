235c07478d09eccfca2e6083b365b52c4d527cb1
mekho007 <44125498+mekho007@users.noreply.github.com>
2021-07-18 01:26:15 +0300

    Add files via upload

---
+++ movement.sql
@@ -0,0 +1,41 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: 12 يوليو 2021 الساعة 17:20
+-- إصدار الخادم: 10.4.19-MariaDB
+-- PHP Version: 8.0.7
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `robot base panel`
+--
+
+-- --------------------------------------------------------
+
+--
+-- بنية الجدول `movement`
+--
+
+CREATE TABLE `movement` (
+  `forward` int(11) NOT NULL,
+  `backward` int(11) NOT NULL,
+  `stop` int(11) NOT NULL,
+  `Left` int(11) NOT NULL,
+  `Right` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
